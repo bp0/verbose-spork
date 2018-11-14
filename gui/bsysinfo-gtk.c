a849ee71ec525a2e03e046d0f4fb94666e30c842
Burt P <pburt0@gmail.com>
2018-11-13 22:21:46 -0600

    add GtkStack support, although I prefer notebook
    Signed-off-by: Burt P <pburt0@gmail.com>

--- gui/bsysinfo-gtk.c
+++ gui/bsysinfo-gtk.c
@@ -22,9 +22,13 @@
 #include <inttypes.h> /* for PRIu64 */
 #include "uber.h"
 
+/* use notebook or stack/switcher (gtk3.10+) */
+#define USE_NOTEBOOK TRUE
+#define PAGE_BROWSER 0 /* when using notebook */
+
 GtkWidget *app_window = NULL;
 GtkWidget *notebook = NULL;
-#define PAGE_BROWSER 0
+GtkWidget *stack = NULL;
 
 #define UPDATE_TIMER_SECONDS 0.2
 
@@ -481,12 +485,24 @@ static void pins_list_view_update(pins_list_view *plv) {
     gtk_tree_model_foreach(GTK_TREE_MODEL(plv->store), pins_list_view_update_row, plv);
 }
 
-static void add_notebook_page(const gchar *label, GtkWidget *notebook, GtkWidget *page_widget, gint border) {
-    GtkWidget *lbl = gtk_label_new (label);
-    gtk_container_set_border_width (GTK_CONTAINER (page_widget), border);
-    gtk_widget_set_size_request (page_widget, 100, 75);
-    gtk_notebook_append_page(GTK_NOTEBOOK (notebook), page_widget, lbl);
-    gtk_widget_show (page_widget);
+static void notebook_goto_page(const gchar *name, int page_num) {
+    if (notebook)
+        gtk_notebook_set_current_page(GTK_NOTEBOOK(notebook), PAGE_BROWSER);
+    else if (stack)
+        gtk_stack_set_visible_child_name(GTK_STACK(stack), name);
+}
+
+static void notebook_add_page(const gchar *name, const gchar *label, GtkWidget *notebook, GtkWidget *page_widget, gint border) {
+    if (notebook) {
+        GtkWidget *lbl = gtk_label_new (label);
+        gtk_container_set_border_width (GTK_CONTAINER (page_widget), border);
+        gtk_widget_set_size_request (page_widget, 100, 75);
+        gtk_notebook_append_page(GTK_NOTEBOOK (notebook), page_widget, lbl);
+        gtk_widget_show (page_widget);
+    } else if (stack) {
+        gtk_stack_add_titled(GTK_STACK(stack), page_widget, name, label);
+        gtk_widget_show(page_widget);
+    }
 }
 
 struct {
@@ -669,7 +685,7 @@ gboolean browser_navigate_actual(gchar *safe) {
                 path, NULL, FALSE);
 
     if (notebook)
-        gtk_notebook_set_current_page(GTK_NOTEBOOK(notebook), PAGE_BROWSER);
+        notebook_goto_page("browser", PAGE_BROWSER);
 
     return G_SOURCE_REMOVE; /* remove from the main loop */
 }
@@ -776,24 +792,45 @@ int main(int argc, char **argv) {
     refresh_timer.interval_ms = (UPDATE_TIMER_SECONDS * 1000.0);
     refresh_timer.timeout_id = g_timeout_add(refresh_timer.interval_ms, refresh_data, NULL);
 
-    /* Set up main window */
-    app_window = gtk_window_new (GTK_WINDOW_TOPLEVEL);
+    /* notebook pages */
+    GtkWidget *blah = NULL;
+    if (USE_NOTEBOOK) {
+        blah = notebook = gtk_notebook_new();
+        gtk_notebook_set_tab_pos(GTK_NOTEBOOK (notebook), GTK_POS_TOP);
+    } else {
+        blah = gtk_box_new(GTK_ORIENTATION_VERTICAL, 5);
+        stack = gtk_stack_new();
+        GtkWidget *switcher = gtk_stack_switcher_new();
+        gtk_stack_switcher_set_stack(GTK_STACK_SWITCHER(switcher), GTK_STACK(stack));
+        gtk_widget_set_halign(switcher, GTK_ALIGN_CENTER);
+        gtk_box_pack_start(GTK_BOX(blah), switcher, FALSE, FALSE, 0);
+        gtk_box_pack_start(GTK_BOX(blah), stack, TRUE, TRUE, 0);
+        gtk_widget_show(switcher);
+        gtk_widget_show(stack);
+    }
+    notebook_add_page("browser", _("Browser"), notebook, gel.container, 5);
+    notebook_add_page("watchlist", _("Watchlist"), notebook, gwl.container, 5);
+    notebook_add_page("about", _("About"), notebook, about, 5);
 
+    /* set up main window */
+    app_window = gtk_window_new (GTK_WINDOW_TOPLEVEL);
     g_signal_connect (app_window, "delete-event",
           G_CALLBACK (delete_event), NULL);
     g_signal_connect (app_window, "destroy",
           G_CALLBACK (destroy), NULL);
 
-    /* Sets the border width of the window. */
-    gtk_container_set_border_width (GTK_CONTAINER (app_window), 10);
+    gtk_container_add(GTK_CONTAINER(app_window), blah);
+    gtk_widget_show(blah);
 
-    /* notebook pages */
-    notebook = gtk_notebook_new();
-    gtk_notebook_set_tab_pos(GTK_NOTEBOOK (notebook), GTK_POS_TOP);
-    add_notebook_page(_("Browser"), notebook, gel.container, 5);
-    add_notebook_page(_("Watchlist"), notebook, gwl.container, 5);
-    add_notebook_page(_("About"), notebook, about, 5);
+    GdkPixbuf *icon = gtk_icon_theme_load_icon(gtk_icon_theme_get_default(), "face-monkey", 128, 0, NULL);
+    if (icon) {
+        gtk_window_set_icon(GTK_WINDOW(app_window), icon);
+        g_object_unref(icon);
+    }
+    gtk_container_set_border_width(GTK_CONTAINER(app_window), 5);
+    gtk_window_resize (GTK_WINDOW (app_window), 740, 400);
 
+    /* get ready */
     if (query)
         browser_navigate(query);
     else
@@ -801,18 +838,8 @@ int main(int argc, char **argv) {
 
     watchlist_load();
 
-    /* This packs the notebook into the window (a gtk container). */
-    gtk_container_add (GTK_CONTAINER (app_window), notebook);
-    gtk_widget_show (notebook);
-
-    gtk_window_resize (GTK_WINDOW (app_window), 740, 400);
-    GdkPixbuf *icon = gtk_icon_theme_load_icon(gtk_icon_theme_get_default(), "face-monkey", 128, 0, NULL);
-    if (icon) {
-        gtk_window_set_icon(GTK_WINDOW(app_window), icon);
-        g_object_unref(icon);
-    }
+    /* go */
     gtk_widget_show (app_window);
-
     gtk_main ();
 
     return 0;
