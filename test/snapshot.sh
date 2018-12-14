d056c63e5321d13f9df53f1507707bb2e65295a2
Burt P <pburt0@gmail.com>
2018-12-14 10:08:54 -0600

    snapshot: add mdstat
    Signed-off-by: Burt P <pburt0@gmail.com>

--- test/snapshot.sh
+++ test/snapshot.sh
@@ -2,24 +2,25 @@
 ZNAME=$(hostname -s);
 echo "Snapshot for $ZNAME..."
 zip -y -r "SS_$ZNAME.zip" \
-	"/sys/" \
-	"/sys/devices/" \
-	"/sys/bus/" \
-	"/sys/block/" \
-	"/sys/class/" \
-	"/sys/firmware/devicetree/base" \
-	"/sys/devices/" \
-	"/sys/devices/system/cpu/" \
-	"/sys/devices/virtual/dmi/id" \
-	"/proc/sys/" \
-	"/proc/devicetree/" \
-	"/proc/cpuinfo" \
-	"/proc/meminfo" \
-	"/proc/stat" \
-	"/proc/uptime" \
-	"/proc/crypto" \
-	"/proc/modules" \
-	"/usr/lib/os-release" \
+	/sys \
+	/sys/devices \
+	/sys/bus \
+	/sys/block \
+	/sys/class \
+	/sys/firmware/devicetree/base \
+	/sys/devices \
+	/sys/devices/system/cpu/ \
+	/sys/devices/virtual/dmi/id \
+	/proc/sys \
+	/proc/devicetree \
+	/proc/cpuinfo \
+	/proc/meminfo \
+	/proc/stat \
+	/proc/mdstat \
+	/proc/uptime \
+	/proc/crypto \
+	/proc/modules \
+	/usr/lib/os-release \
 	/etc/issue \
 	/etc/*-release \
 	/etc/*-version \
